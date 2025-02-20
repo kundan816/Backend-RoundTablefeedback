8bfb225ac1a75542eea4c1ee5d38446cef2579c0
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2025-02-20 12:42:18 -0800

    Initial commit

---
+++ RtFeedbackkkkkApplication.java
@@ -0,0 +1,13 @@
+package com.assignm4.RTFeedbackkkkk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtFeedbackkkkkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtFeedbackkkkkApplication.class, args);
+	}
+
+}
