0315177171928ce631deab8290a5b913af6f1531
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2025-02-20 14:03:49 -0800

    Repository added

--- Repository/AdminOverrideRepository.java
+++ Repository/AdminOverrideRepository.java
@@ -1,8 +1,8 @@
 package com.assignm4.RTFeedbackkkkk.Repository;
 
-import com.assignm4.RTFeedbackkkkk.enitity.FeedbackForm;
+import com.assignm4.RTFeedbackkkkk.enitity.AdminOverride;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FeedbackFormRepository extends JpaRepository<FeedbackForm, Long> {}
+public interface AdminOverrideRepository extends JpaRepository<AdminOverride, Long> {}
